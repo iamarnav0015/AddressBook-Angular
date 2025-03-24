bc1c4ac5aabbaa25838039400919e35ecbffd08c
Arnav@Victor <shubhamsaxena28282@gmail.com>
2025-03-24 15:34:46 +0530

    [Arnav] Validate User Input (Min 3 Letters & Initial Caps)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 export class AppComponent {
   title = 'Hello from BridgeLabz';
   userName: string = '';
+
+  isValidName(): boolean {
+    return /^[A-Z][a-zA-Z]{2,}$/.test(this.userName);
+  }
 }
