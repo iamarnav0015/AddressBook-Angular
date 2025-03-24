046c5021bd692150e9b73367afe76d11e264f92b
Arnav@Victor <shubhamsaxena28282@gmail.com>
2025-03-24 15:26:17 +0530

    [Arnav]Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 export class AppComponent {
   title = 'Hello from BridgeLabz';
   imgUrl = 'https://www.bridgelabz.com/images/BridgeLabz%20Logo.svg';
+
+  openBridgeLabzSite() {
+    window.open('https://www.bridgelabz.com', '_blank');
+  }
 }
