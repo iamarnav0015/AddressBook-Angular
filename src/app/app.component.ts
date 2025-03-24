6e92c582547ac9833a3115bc826fd345c3c032f6
Arnav@Victor <shubhamsaxena28282@gmail.com>
2025-03-24 15:22:59 +0530

    [Arnav] Logo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
 export class AppComponent {
   title = 'Hello from BridgeLabz';
+  imgUrl = 'https://www.bridgelabz.com/images/BridgeLabz%20Logo.svg';
 }
