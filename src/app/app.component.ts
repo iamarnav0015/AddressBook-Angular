5fc8e93b144785843f229dddbf70f3c3048069cb
Arnav@Victor <shubhamsaxena28282@gmail.com>
2025-03-24 15:28:42 +0530

    [Arnav] Display User Inputted Name using Two-Way Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,4 @@
 export class AppComponent {
   title = 'Hello from BridgeLabz';
-  imgUrl = 'https://www.bridgelabz.com/images/BridgeLabz%20Logo.svg';
-
-  openBridgeLabzSite() {
-    window.open('https://www.bridgelabz.com', '_blank');
-  }
+  userName: string = '';
 }
